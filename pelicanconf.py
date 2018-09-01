f516f41dfb603e92b16c99d0590d08a9d9e4832c
Sean Ammirati <sean.ammirati75@myhunter.cuny.edu>
2018-09-01 01:49:44 -0400

    Added skeleton

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,18 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import bulrush
 
 AUTHOR = 'Sean Ammirati'
 SITENAME = 'Stats Works'
 SITESUBTITLE = 'Making Statistics Work for You'
-SITEURL = ''
+SITEURL = 'statsworks.github.io.git'
+SIDEBAR_NAME = AUTHOR
+SIDEBAR_EMAIL = 'ammirati.sean@gmail.com'
+SIDEBAR_TAGS = ['Statistics',
+                'Data Science',
+                'Machine Learning',
+                'Python',
+                'R'
+                ]
+MENUITEMS = [('Home', '/'),
+             ('Topics', '/pages/topics'),
+             ('About', 'pages/about'),
+             ]
+
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
 
 PATH = 'content'
+
+MARKUP = ('md', 'ipynb')
+PLUGIN_PATHS = ['./plugins', './pelican-plugins']
+PLUGINS = ['ipynb.markup', 'render_math', 'better_codeblock_line_numbering',
+           'assets']
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5'
+}
+
+ARTICLE_URL = 'articles/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
 
 TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = 'English'
 
+THEME = bulrush.PATH
+JINJA_ENVIRONMENT = bulrush.ENVIRONMENT
+JINJA_FILTERS = bulrush.FILTERS
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
